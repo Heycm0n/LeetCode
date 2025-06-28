6ee03492af042bfd0ee5c15fb6b8731355b49535
Heycm0n <pascha.botsman@yandex.ru>
2025-06-28 13:15:02 +0300

    20. Valid Parentheses
    https://leetcode.com/problems/valid-parentheses/description/

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -5,101 +5,26 @@ using System.Globalization;
 
 public class Solution
 {
-    public List<string> _Solutions;
-    public void DivideSubStrings(string str, int char_del_num, string s, int k) 
+    public bool IsValid(string s)
     {
-        if (CountSubStr(str, s) == k)
+        Stack<char> chars = new Stack<char>();
+        foreach (char c in s)
         {
-            //return str;
-            if(!_Solutions.Contains(str)) _Solutions.Add(str);
+            if (c == '(') chars.Push(')');
+            else if (c == '[') chars.Push(']');
+            else if (c == '{') chars.Push('}');
+            else if ((chars.Count == 0) || (chars.Pop() != c)) return false;
         }
-        else 
-        {        
-            string substr = str.Remove(char_del_num, 1); //Получаем новую подстроку. Проверяем ее.
-            if (substr.Length < 2) return;
-            
-            for (int i = 0; i < substr.Length; i++) 
-            {
-                DivideSubStrings(substr, i, s, k);  
-            }         
-        }
-        return;
+        if (chars.Count == 0) return true;
+        else return false;
     }
-    public int CountSubStr(string substr, string s) 
-    {
-        int substr_char_num = 0;
-        int Count = 0;
-        for (int i = 0; i < s.Length; i++) 
-        {
-            if (s[i] == substr[substr_char_num]) 
-                if (++substr_char_num >= substr.Length) 
-                {
-                    substr_char_num = 0;
-                    Count++;
-                } 
-        }
-        return Count;
-    }
-    public string LongestSubsequenceRepeatedK(string s, int k)
-    {
-        _Solutions = new List<string>();
-        Dictionary<char, int> charlist = new Dictionary<char, int>();
-
-        //Удаляем лишние буквы.
-        for (int i = 0; i < s.Length; i++)
-        {
-            charlist.TryGetValue(s[i], out int num);
-            if (num == 0)
-                charlist.Add(s[i], 1);
-            else
-                charlist[s[i]] += 1;
-        }
-
-
-        foreach (char c in charlist.Keys)
-        {
-            if(charlist[c] < k)
-                for (int i = 0; i < s.Length; i++) 
-                {
-                    if (s[i] == c) 
-                        s = s.Remove(i, 1);
-                }
-        }
-
-        if (s.Length < k) return string.Empty;
-        //Основной цикл следит за размером последовательности
-
-        int substr_len = s.Length - (k - 1);
-        string substr = new string("");
-        substr = s.Substring(0, substr_len);    //Смотрим самую большую последовательность
-        for (int i = 0; i < substr.Length; i++)
-        {
-            DivideSubStrings(substr, i, s, k);
-        }
-
-        if (_Solutions.Count == 0)
-            return string.Empty;
-        else 
-        {
-            string true_solution = string.Empty;
-            for (int i = 0; i < _Solutions.Count; i++) 
-            {
-                if (_Solutions[i].Length > true_solution.Length) true_solution = _Solutions[i];
-                else if ((_Solutions[i].Length == true_solution.Length)&&(string.Compare(_Solutions[i], true_solution) > 0)) 
-                    true_solution = _Solutions[i];
-            }
-            return true_solution;
-        }
-    }
-
     static void Main(string[] args)
     {
         Solution solution = new Solution();
 
-        string str = "letsleetcode";
-        int k = 2;
+        string str = "{}()[]{}";
 
-        Console.WriteLine(solution.LongestSubsequenceRepeatedK(str, k));
+        Console.WriteLine(solution.IsValid(str));
     }
 }
 
