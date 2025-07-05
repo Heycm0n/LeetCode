e7a68bd0b46db615771e3a8be033ce755479ad46
Heycm0n <pascha.botsman@yandex.ru>
2025-07-05 21:12:44 +0300

    717. 1-bit and 2-bit Characters
    https://leetcode.com/problems/1-bit-and-2-bit-characters/description/

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,41 +4,45 @@ using Microsoft.Extensions.Primitives;
 using Microsoft.VisualBasic;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
 using System.Globalization;
 
 public class Solution
 {
-    public int max_sub = 0;
-    void func(int i, int j, int[] nums1, int[] nums2, int row) 
-    {     
-        if (((i-row) < 0) || ((j - row) < 0)) return; //Выходим если доходим до конца
-        if (nums1[i - row] == nums2[j - row])
-        {
-            row++;
-            if (row > max_sub) max_sub = row;
-            func(i, j, nums1, nums2, row);
-        }
-        return;
-    }
-    public int FindLength(int[] nums1, int[] nums2)
+    public bool IsOneBitCharacter(int[] bits)
     {
-        for (int i = nums1.Length - 1; i >= 0; i--)
-            for (int j = nums2.Length - 1; j >= 0; j--)
+        //если две единицы встречаются в тексте то это точно 11, то есть удалив все 11 из текста мы гарантируем что в тексте остались только 10 и 0
+        //если после этого в тексте встречаются 2 и более подряд 0 то это значит что это 10 + 0, значит можно заменить все повторяющиеся 0 одним. (кроме случая когда они в самом начале строки)
+        //потом в строке должно остаться только 10
+
+        //Либо просто находим следующй за этим ноль. Если он следующий то это точно один бит. Если он через четное количество 1 то тоже 0, нечетное то 10.
+        int counter = 0;
+        for (int i = bits.Length - 2; i >= 0; i--) 
+        {
+            if (bits[i] == 0)
             {
-                func(i, j, nums1, nums2, 0);
-                if (max_sub > i) break;
+                break;
             }
-           
-        return max_sub;
+            else 
+            {
+                counter++;
+            }
+        }
+
+        if (counter % 2 != 0) 
+            return false;
+        else 
+            return true;
+        
     }
+
     static void Main(string[] args)
     {
         Solution solution = new Solution();
 
         //string[] words = { "a", "banana", "app", "appl", "ap", "apply", "apple" };
-        int[] nums1 = { 1, 1, 0, 0, 1 };
-        int[] nums2 = { 1, 1, 0, 0, 0 };
-        int answ = solution.FindLength(nums1, nums2);
+        int[] nums = { 1, 1, 1, 0 };
+        bool answ = solution.IsOneBitCharacter(nums);
         Console.WriteLine(answ);
 
     }
